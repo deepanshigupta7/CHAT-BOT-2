fd05f6009633e6b44ae63a5ff60ad8de1671b6a2
Deepanshi Gupta <deepanshi1007@gmail.com>
2023-06-22 22:04:03 +0530

    index.html

--- static/Scripts/responses.js
+++ static/Scripts/responses.js
@@ -1,11 +1,19 @@
-async function fetchMessages() {
+const emotionMapping = {
+  Angry: "anger",
+  Fear: "sadness",
+  Happy: "joy",
+  Sad: "sadness",
+  Surprise: "neutral",
+}
+
+async function fetchMessages(emotion) {
   try {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     var raw = JSON.stringify({
       context: responses.slice(-3),
-      emotion: "joy",
+      emotion: emotionMapping[emotion],
     });
 
     var requestOptions = {
@@ -26,8 +34,38 @@ async function fetchMessages() {
   }
 }
 
+async function getEmotion(text){
+  try{
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "text": text
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+const response = await fetch(
+      "http://192.168.29.11:3000/emotion",
+      requestOptions
+    );
+    const result = await response.text();
+    return result;
+
+  }catch(err) {
+    return err;
+  }
+}
+
 async function getBotResponse(input) {
-  const res = await fetchMessages();
+  const emotion_json = await getEmotion(input);
+  const emotion = JSON.parse(emotion_json).emotion;
+  const res = await fetchMessages(emotion);
   const responseMessage = JSON.parse(res).response;
   responses.push(responseMessage);
   return responseMessage;
